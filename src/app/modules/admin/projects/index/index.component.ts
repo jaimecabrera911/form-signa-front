9c27a9a624e0ac3c92d22c4d7c1829bdd00e7a7e
Miguel Espejo <migueltf11cm@gmail.com>
2024-12-01 20:16:52 -0500

    Ajustes endpoint, modulo proyectos

--- src/app/modules/admin/projects/index/index.component.ts
+++ src/app/modules/admin/projects/index/index.component.ts
@@ -15,21 +15,18 @@ export class IndexComponent  implements OnInit {
     searchPanel: boolean = true;
     apiItems$: Observable<any>;
     iterableColumns: TableItems[] = [
-        { name: 'code', name2: false, styleEnable:false, label: 'Número' },
+        { name: 'uid', name2: false, styleEnable:false, label: 'Número' },
         { name: 'name', name2: false, styleEnable:false, label: 'Nombre' },
-        { name: 'startDate', name2: false, styleEnable:false, label: 'Fecha creación' },
-        { name: 'endDate', name2: false, styleEnable:false, label: 'Fecha finalización' },
-        { name: 'city', name2: 'name', styleEnable:false, label: 'Ubicación' },
-        { name: 'employee', name2: 'username', styleEnable:false, label: 'Responsable' },
-        { name: 'state', name2: 'name', styleEnable:false, label: 'Estado', styles: [
-                { label: 'En Ejecucion', textColor: '#A229CC', backgroundColor: '#F0D7F9' },
-                { label: 'Por iniciar', textColor: '#0F4EC9', backgroundColor: '#AAC1FE' },
-                { label: 'Terminado', textColor: '#0D7D62', backgroundColor: '#D5F0E0' },
-                { label: 'Suspendido', textColor: '#C92C2C', backgroundColor: '#F4B2B6' }
+        { name: 'startDate', name2: false, formatDate: true, styleEnable:false, label: 'Fecha creación' },
+        { name: 'endDate', name2: false, formatDate: true, styleEnable:false, label: 'Fecha finalización' },
+        { name: 'status', name2: false, styleEnable:false, label: 'Estado', styles: [
+                { label: 'active', textColor: '#A229CC', backgroundColor: '#F0D7F9' },
+                { label: 'pending', textColor: '#0F4EC9', backgroundColor: '#AAC1FE' },
+                { label: 'completed', textColor: '#0D7D62', backgroundColor: '#D5F0E0' }
             ]
         },
-        { name: 'view', name2: false, id: 'id', styleEnable: false, label: 'Ver', view: true, editModule: 'projects' },
-        { name: 'edit', name2: false, id: 'id', styleEnable: false, label: 'Editar', edit: true, editModule: 'projects' }
+        { name: 'view', name2: false, id: 'uid', styleEnable: false, label: 'Ver', view: true, editModule: 'projects' },
+        { name: 'edit', name2: false, id: 'uid', styleEnable: false, label: 'Editar', edit: true, editModule: 'projects' }
     ];
 
     constructor(
@@ -38,6 +35,6 @@ export class IndexComponent  implements OnInit {
     }
 
     ngOnInit(): void {
-        this.apiItems$ = this.api.projectService(1);
+        this.apiItems$ = this.api.projectService();
     }
 }
